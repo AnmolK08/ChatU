61c896cd27bcae601bf7d0059d6f8636dbc74995
AnmolK08 <mranmolsingh04@gmail.com>
2025-08-04 09:35:03 +0530

    Integrate the login signup and logout api

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import assets, { userDummyData } from "../assets/assets";
+import { AuthContext } from "../../context/AuthContext";
 
+const SideBar = ({ selectedUser, setSelectedUser }) => {
+  const { logout } = useContext(AuthContext);
 
-const SideBar = ({selectedUser , setSelectedUser}) => {
-  
   const navigate = useNavigate();
   return (
     <div
@@ -29,7 +30,9 @@ const SideBar = ({selectedUser , setSelectedUser}) => {
                 Edit Profile
               </p>
               <hr className="my-2 border-t border-gray-500" />
-              <p className="cursor-pointer text-sm">Logout</p>
+              <p onClick={()=>{logout}} className="cursor-pointer text-sm">
+                Logout
+              </p>
             </div>
           </div>
         </div>
--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import assets from '../assets/assets';
+import { AuthContext } from '../../context/AuthContext';
 
 const ProfilePage = () => {
 
   const [selectedImg, setSelectedImg] = useState(null);
   const navigate = useNavigate();
-  const [name, setName] = useState("MartinJohnson");
-  const [bio, setBio] = useState("Hi , I am using ChatU");
+  const [name, setName] = useState(authUser.fullName);
+  const [bio, setBio] = useState(authUser.bio);
+  const { authUser , updateProfile } = useContext(AuthContext);
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
-    navigate('/');
+    if(!selectedImg){
+      await updateProfile({fullName : name , bio});
+      navigate('/');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsDataURL(selectedImg);
+    reader.onload = async()=>{
+      const base64Image = reader.result;
+      await updateProfile({profilePic: base64Image , fullName : name , bio});
+      navigate('/');
+    }
+
+    
   }
 
   return (
@@ -40,7 +56,7 @@ const ProfilePage = () => {
 
         </form>
 
-        <img src={assets.logo_icon} alt="" className='max-w-44 aspect-square rounded-full max-10 max-sm:mt-10'/>
+        <img src={authUser.profilePic || assets.logo_icon} alt="" className={`max-w-44 aspect-square rounded-full max-10 max-sm:mt-10 ${selectedImg && 'rounded-full'}`}/>
       </div>
 
     </div>
